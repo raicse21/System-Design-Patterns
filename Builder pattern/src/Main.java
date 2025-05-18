d129b1dfe778381067e5529ba7032aa440704f84
raicse21 <rishikeshrai212012@gmai.com>
2025-05-19 00:09:50 +0530

    Additional comments

--- Builder pattern/src/Main.java
+++ Builder pattern/src/Main.java
@@ -105,7 +105,39 @@ public class Main {
 }
 
 /*
-This can be achieved by @Builder annotation
+This can be achieved by @Builder annotation -> defined in Lombok
+It internally generates
+1. A static inner class called Builder
+2. Setter-like methods in builder class for  each field
+3. A build() method that can create main object
+4. A builder() static method in Order class to get an instance of builder
 
+In case of annotation code will be like:-
+
+Order order = new Order.builder()
+                .id(21)
+                .customerName("Rishikesh Rai")
+                .shoppingAddress("Delhi")
+                .build();
+
+
+Advantages of Builder pattern:-
+1. Immutability:
+    By using a Private constructor and only allowing object creation through
+    the builder, you ensure that the object is immutable once it is built.
+
+2. Readability and Maintainability:
+    The builder's method chaining makes it easy to read and understand the code, and the
+    builder pattern improves maintainability by keeping the creation process separate from the
+    object's implementation.
+
+3. Flexibility with Optional fields:
+    The builder pattern handles optional fields effectively.You can choose which fields to
+    set without needing to worry about the order of parameters or overloading the
+    constructors with many parameters.
+
+4. Preventing Constructor Overloading
+    Instead of having multiple constructors with different parameter combinations, the builder
+    pattern provides a more flexible and clean way to manage different object configuration.
 
 */
